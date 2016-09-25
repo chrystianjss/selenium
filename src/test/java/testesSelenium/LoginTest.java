95d64590b46fc988194616c593a89d715b297bfe
chrystianjss <chrystian.sqft@gmail.com>
2016-09-25 17:10:38 -0300

    Ajuste realizados na classe de teste do Selenium

--- src/test/java/testesSelenium/LoginTest.java
+++ src/test/java/testesSelenium/LoginTest.java
@@ -3,27 +3,24 @@ package testesSelenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class LoginTest  {
-    public static void main(String[] args) {
-    	System.setProperty("webdriver.edge.driver", "C:\\Users\\Chrystian Soares\\Downloads\\MicrosoftWebDriver.exe");
-    	//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Chrystian Soares\\Downloads\\chromedriver.exe");
+    public static void main(String[] args) throws InterruptedException {
     	WebDriver driver = new EdgeDriver();
     	
         driver.get("http://localhost:8080/selenium/login.xhtml");
         
-        //WebElement element = driver.findElement(By.id("j_idt6:inputNome"));
         WebElement element = driver.findElement(By.xpath("//*[contains(@id, 'inputLogin')]"));
         element.sendKeys("admin");
         
-        //element = driver.findElement(By.id("inputSenha"));
         element = driver.findElement(By.xpath("//*[contains(@id, 'inputSenha')]"));
-        element.sendKeys("123");
-        
-        element.submit();
+        element.sendKeys("321");
 
-        System.out.println("Page title is: " + driver.getTitle());
-        //driver.quit();
+        element.submit();
+        
+        Thread.sleep(1500);
+        driver.quit();
     }
 }
